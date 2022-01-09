904a6f763c3999fd3215526246af11f2bb56523c
Ishan <ishan28mkip@gmail.com>
2022-01-09 22:26:30 +0530

    added antd and layout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+// next.config.js
+const withAntdLess = require("next-plugin-antd-less");
+
+module.exports = withAntdLess({
+  // modifyVars: { "@primary-color": "#04f" }, // optional
+  // lessVarsFilePath: "./src/styles/variables.less", // optional
+  // lessVarsFilePathAppendToEndOfContent: false, // optional
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {
+    // mode: "local",
+    // exportLocalsConvention: "camelCase",
+    // exportOnlyLocals: false,
+  },
   reactStrictMode: true,
-}
+
+  webpack(config) {
+    return config;
+  },
+});
